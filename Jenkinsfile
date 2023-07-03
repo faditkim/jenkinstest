9130621776ee980487f0dccfe34251ac3e48d376
Taemin Kim <taemin.kim@fashion-digital.de>
2023-07-03 11:41:35 +0200

    test1

--- Jenkinsfile
+++ Jenkinsfile
@@ -1,3 +1,19 @@
-node {
-    load 'jenkins/sub1.groovy'
+pipeline {
+    // don't use 'agent any'
+    // 'agent none' allows the job to continue on the same node with the same files
+    agent any
+
+    stages {
+        stage('Greetings') {
+            steps {
+                script {
+                    sh "echo Hello $MY_ENV"
+                }
+            }
+        }
+
+        stage ('load')
+            load './test2/jenkinstest/jenkins/sub1.groovy'
+
+    }
 }
--- jenkins/sub1.groovy
+++ jenkins/sub1.groovy
@@ -1,25 +1,36 @@
+//pipeline {
+//    // don't use 'agent any'
+//    // 'agent none' allows the job to continue on the same node with the same files
+//    agent none
+//
+//    stages {
+//
+//        stage('List Files') {
+//            steps {
+//                script {
+//                    sh "ls -al"
+//                }
+//            }
+//        }
+//
+//        stage('Greetings') {
+//            steps {
+//                script {
+//                    sh "echo Hello $MY_ENV"
+//                }
+//            }
+//        }
+//
+//    }
+//}
+
 pipeline {
-    // don't use 'agent any'
-    // 'agent none' allows the job to continue on the same node with the same files
     agent none
-
-    stages {
-
-        stage('List Files') {
-            steps {
-                script {
-                    sh "ls -al"
-                }
+    stage('List Files') {
+        steps {
+            script {
+                sh "ls -al"
             }
         }
-
-        stage('Greetings') {
-            steps {
-                script {
-                    sh "echo Hello $MY_ENV"
-                }
-            }
-        }
-
     }
 }
